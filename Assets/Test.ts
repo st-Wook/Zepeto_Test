7926e111d9548858679588d3dd61b02938a8267b
st-Wook <phk09242@supertree.co>
2021-11-03 18:08:32 +0900

    Test 스크립트 수정 및 컴파일

--- Assets/Test.ts
+++ Assets/Test.ts
@@ -2,15 +2,17 @@ import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
 
 export default class Test extends ZepetoScriptBehaviour {
     Start() {
+        console.log("START")
         this.TestFun();
     }
 
     TestFun() {
-        console.log("LOG")
-        const promise = new Promise((resolve, reject) => {
-            setTimeout(() => resolve("TETE"), 5000)
+        console.log("TEST FUN")
+        const promise = new Promise((resolve, _) => {
+            setTimeout(() => resolve("DELAY TEST"), 5000)
         })
         promise.then((res) => {
+            console.log(res)
         })
     }
 }
